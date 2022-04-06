a9dc828218a0dced3ad39e14f4c8e528c0954263
Adriano <adriano.selaviles@gmail.com>
2022-04-05 20:08:13 -0700

    simple initial otp implementation

--- contracts/VerifiableOTP.sol
+++ contracts/VerifiableOTP.sol
@@ -5,8 +5,39 @@ import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
 contract VerifiableOTP is AccessControlEnumerable {
 
+    mapping(bytes32 => bool) public _answers;
+
+    event OneTimePasscodeAnswerPublished(
+        bytes32 answer
+    );
+
+    event OneTimePasscodeAnswerVerified(
+        address verifiedBy,
+        bytes32 answer
+    );
+
     constructor() {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function publishAnswer(bytes32 answer) external {
+        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()));
+        _answers[answer] = true;
+        emit OneTimePasscodeAnswerPublished(answer);
+    }
+
+    function checkAnswer(string memory otp) external returns (bool) {
+        require(bytes(otp).length == 64);
+
+        bytes32 ans = keccak256(abi.encodePacked(otp));
+
+        if (!_answers[ans]) {
+            return false;
+        }
+
+        delete _answers[ans];
+        emit OneTimePasscodeAnswerVerified(_msgSender(), ans);
+        return true;
     }
 
 }
